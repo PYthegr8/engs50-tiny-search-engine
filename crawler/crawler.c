b64d2eb2cd35faf1fc04ec03c01f2cc56ebb12bb
gentmaks <gent.maksutajmax@gmail.com>
2025-10-19 06:26:27 -0400

    hash impl done

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -18,6 +18,8 @@
 
 void print_q_contents(void* elementp);
 void free_page(void* elementp);
+bool key_exist(hashtable_t *htp, void* keyp);
+bool equals(void* elementp, const void* keyp);
 
 int main(){
     // Module 4 Step 2
@@ -72,6 +74,30 @@ int main(){
     qclose(q);
 
     // Module 4 step 4
+    printf("Using a hashtable to eliminte duplicate URLs\n");
+    webpage_t* page_step4 = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
+    hashtable_t *lookup = hopen(1000);
+
+    if(webpage_fetch(page_step4)) {
+        
+        int pos = 0;
+        char *result;
+        while ((pos = webpage_getNextURL(page_step4, pos, &result)) > 0) {
+            printf("This is the current URL we are getting: %s\n", result);
+            if (IsInternalURL(result) && !key_exist(lookup, result)) {
+                webpage_t* new_page = webpage_new(result, 0, NULL);
+                if (hput(lookup, (void*)new_page, result, sizeof(result))) {
+                    fprintf(stderr, "[Error: Could not insert the URL into the queue]\n"); 
+                    return EXIT_FAILURE;
+                }
+            }
+            free(result);
+        }
+        happly(lookup, print_q_contents);
+    }
+    webpage_delete(page_step4);
+    happly(lookup, free_page);
+    hclose(lookup);
 }
 
 
@@ -83,5 +109,27 @@ void print_q_contents(void* elementp) {
 void free_page(void* elementp) {
     webpage_t* page = (webpage_t*)elementp;
     webpage_delete(page);
+}
+
+bool key_exist(hashtable_t *htp, void* keyp) {
+    if ((hsearch(htp, equals, keyp, sizeof(keyp))) == NULL) {
+        return false; 
+    }
+    return true;
+}
+
+bool equals(void* elementp, const void* keyp) {
+    webpage_t* page = (webpage_t*)elementp;
+    const char* url = webpage_getURL(page);
+    const char* keyURL = (const char*)keyp;
+
+    printf("URL we're passing in is: %s\n", url);
+    printf("URL we are comparing with is: %s\n", keyURL);
+
+    if (strcmp(url, keyURL) == 0) {  // compare string contents
+        return true;
+    }
+
+    return false;
 }
 

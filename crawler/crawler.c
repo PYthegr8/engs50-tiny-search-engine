95ea6866fac99e991842b71c4f79d03411274429
Papa Yaw <papa.yaw.owusu.nti.ug@dartmouth.edu>
2025-10-18 20:04:13 -0400

    Updating crawler file

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -5,15 +5,40 @@
  * Created: 10-17-2025
  * Version: 1.0
  * 
- * Description: prints Hello 
+ * Description: This program retrieves the  HTML page from  https://thayer.github.io/engs50/ and prints out all the URLs it contains
  * 
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <webpage.h>
 
 int main(){
-	printf("Hello\n");
-	return 0;
+    webpage_t* page = webpage_new("https://thayer.github.io/engs50/", 0, NULL);
+    if(webpage_fetch(page)) {
+
+        char *html = webpage_getHTML(page);
+        printf("Found html: %s\n", html);
+
+
+        //finding URLs in page
+        int pos = 0;
+        char *result;
+        while ((pos = webpage_getNextURL(page, pos, &result)) > 0) {
+            (IsInternalURL(result)) ? printf("Found an internal URL: %s\n", result) : printf("Found an external URL: %s\n", result);
+            free(result);
+         }
+
+        //deallocating webpage
+        webpage_delete(page);
+        return EXIT_SUCCESS;
+    }
+
+    else {
+        printf("Could not fetch webpage\n");
+        return EXIT_FAILURE;
+    }
 }
 
 

ef20ad1e3bc5fcdbc93db78a8286eeb122e72ed6
gentmaks <gent.maksutajmax@gmail.com>
2025-11-05 23:21:15 -0500

    step 3 starter code

--- querier/query.c
+++ querier/query.c
@@ -18,11 +18,15 @@ Module 6: Querier
 #include "pageio.h"
 #include "indexio.h"
 
+static hashtable_t *g_inverted_index = NULL;
+static char *g_current_word = NULL;
 char *NormalizeWord(char *input);
 void print_q_contents(void* elementp);
 static bool word_equals(void *elementp, const void *keyp);
 static bool doc_equals(void *elementp, const void *keyp);
 static bool build_query_string(hashtable_t *ht, char *query_string, char **words, int count);
+void get_doc_ids(hashtable_t *ht, char *word);
+hashtable_t *init_inverted_index(hashtable_t *ht);
 
 typedef struct {
     char *word;
@@ -43,22 +47,19 @@ int main(void) {
     char line[1000];
     char *file_name = "mod6_step2_index";
     hashtable_t *ht = index_load(file_name);
+    hashtable_t inverted_index = init_inverted_index(ht);
 
     printf("Please enter your input to the command line!\n");
 
     while (1) {
         printf("> ");
-
         if (fgets(line, sizeof(line), stdin) == NULL) {
             printf("End of file signal entered!\n");
             break;
         }
 
         line[strcspn(line, "\n")] = '\0';
-
-        if (strlen(line) == 0) {
-            continue;
-        }
+        if (strlen(line) == 0) continue;
 
         char *token = strtok(line, " \t");
         if (!token) continue;
@@ -87,16 +88,18 @@ int main(void) {
                 free(words[i]);
             continue;
         }
-        char *query_string = (char *)malloc(10000*sizeof(char));
+
+        char *query_string = malloc(10000 * sizeof(char));
         query_string[0] = '\0';
+
         if (build_query_string(ht, query_string, words, count)) {
             printf("%s\n", query_string);
             free(query_string);
-        }
-        else {
+        } else {
             printf("No matches found for your query!\n");
         }
     }
+
     return 0;
 }
 
@@ -119,14 +122,12 @@ char *NormalizeWord(char *input) {
     return newWord;
 }
 
-static bool word_equals(void *elementp, const void *keyp)
-{
+static bool word_equals(void *elementp, const void *keyp) {
     const wordentry_t *we = (const wordentry_t *)elementp;
     return strcmp(we->word, (const char *)keyp) == 0;
 }
 
-static bool doc_equals(void *elementp, const void *keyp)
-{
+static bool doc_equals(void *elementp, const void *keyp) {
     const posting_t *p = (const posting_t *)elementp;
     return p->docID == *(const int *)keyp;
 }
@@ -135,7 +136,6 @@ static bool build_query_string(hashtable_t *ht, char *query_string, char **words
     int lowest = INT_MAX;
     int word_match_count = 0;
     for (int i = 0; i < count; i++) {
-        // words[i] -> word at index i
         int keylen = (int)strlen(words[i]) + 1;
         int docID = 1;
         wordentry_t *we = hsearch(ht, word_equals, words[i], keylen);
@@ -144,9 +144,9 @@ static bool build_query_string(hashtable_t *ht, char *query_string, char **words
             if (p) {
                 word_match_count++;
                 char buffer[20];
-                int count = p->count;
-                sprintf(buffer, "%d", count);
-                lowest = fmin(lowest, count); 
+                int c = p->count;
+                sprintf(buffer, "%d", c);
+                lowest = fmin(lowest, c);
                 strcat(query_string, words[i]);
                 strcat(query_string, ":");
                 strcat(query_string, buffer);
@@ -161,3 +161,34 @@ static bool build_query_string(hashtable_t *ht, char *query_string, char **words
     strcat(query_string, lowest_buffer);
     return word_match_count ? true : false;
 }
+
+static void process_posting(void *postp) {
+    posting_t *p = (posting_t *)postp;
+    char key[20];
+    sprintf(key, "%d", p->docID);
+
+    queue_t *doc_words = hsearch(g_inverted_index, NULL, key, strlen(key) + 1);
+    if (!doc_words) {
+        doc_words = qopen();
+        hput(g_inverted_index, doc_words, key, strlen(key) + 1);
+    }
+
+    wordcount_t *wc = malloc(sizeof(wordcount_t));
+    wc->word = malloc(strlen(g_current_word) + 1);
+    strcpy(wc->word, g_current_word);
+    wc->frequency = p->count;
+    qput(doc_words, wc);
+}
+
+static void process_word(void *elementp) {
+    wordentry_t *we = (wordentry_t *)elementp;
+    g_current_word = we->word;
+    qapply(we->plist, process_posting);
+}
+
+hashtable_t *init_inverted_index(hashtable_t *ht) {
+    g_inverted_index = hopen(1000);
+    happly(ht, process_word);
+    return g_inverted_index;
+}
+

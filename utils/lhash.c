5dfc32b95374d5340515397ee98f97fc4d11520f
Papa Yaw <papayawowusunti@gmail.com>
2025-11-10 15:08:33 -0500

    adding locked hash table module

---
+++ utils/lhash.c
@@ -0,0 +1,58 @@
+/*
+ * hash.c -- implements a locked hash table as an indexed set of locked queues.
+ * updated by Gent Maksutaj, Papa Yaw Owusu Nti, Benjamin Rippy
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include "queue.h"
+#include "hash.h"
+#include "lhash.h"
+
+
+#define get16bits(d) (*((const uint16_t *) (d)))
+
+static uint32_t SuperFastHash(const char *data, int len, uint32_t tablesize) {
+    uint32_t hash = len, tmp;
+    int rem;
+
+    if (len <= 0 || data == NULL)
+        return 0;
+
+    rem = len & 3;
+    len >>= 2;
+
+    for (; len > 0; len--) {
+        hash += get16bits(data);
+        tmp = (get16bits(data + 2) << 11) ^ hash;
+        hash = (hash << 16) ^ tmp;
+        data += 2 * sizeof(uint16_t);
+        hash += hash >> 11;
+    }
+
+    switch (rem) {
+        case 3: hash += get16bits(data);
+                hash ^= hash << 16;
+                hash ^= data[sizeof(uint16_t)] << 18;
+                hash += hash >> 11;
+                break;
+        case 2: hash += get16bits(data);
+                hash ^= hash << 11;
+                hash += hash >> 17;
+                break;
+        case 1: hash += *data;
+                hash ^= hash << 10;
+                hash += hash >> 1;
+    }
+
+    hash ^= hash << 3;
+    hash += hash >> 5;
+    hash ^= hash << 4;
+    hash += hash >> 17;
+    hash ^= hash << 25;
+    hash += hash >> 6;
+    return hash % tablesize;
+}
---
+++ utils/lhash.h
@@ -0,0 +1,49 @@
+#pragma once
+/*
+ * lhash.h -- A locked hash table implementation
+ * Authors: Team MergeConflict
+ *
+ */
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef void lhashtable_t;	/* representation of a locked hashtable hidden */
+
+/* lhopen -- opens a locked hash table with initial size hsize */
+hashtable_t *lhopen(uint32_t hsize);
+
+
+/* lhclose -- closes a locked hash table */
+void lhclose(lhashtable_t *htp);
+
+
+/* lhput -- puts an entry into a locked hash table under designated key
+ * returns 0 for success; non-zero otherwise
+ */
+int32_t lhput(lhashtable_t *htp, void *ep, const char *key, int keylen);
+
+
+/* lhapply -- applies a function to every entry in hash table */
+void happly(lhashtable_t *htp, void (*fn)(void* ep));
+
+
+/* lhsearch -- searchs for an entry under a designated key using a
+ * designated search fn -- returns a pointer to the entry or NULL if
+ * not found
+ */
+void *lhsearch(lhashtable_t *htp,
+	      bool (*searchfn)(void* elementp, const void* searchkeyp),
+	      const char *key,
+	      int32_t keylen);
+
+
+/* lhremove -- removes and returns an entry under a designated key
+ * using a designated search fn -- returns a pointer to the entry or
+ * NULL if not found
+ */
+void *lhremove(hashtable_t *htp,
+ bool (*searchfn)(void* elementp, const void* searchkeyp),
+	      const char *key,
+	      int32_t keylen);
+
